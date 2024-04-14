b551dd790cc8a7dc3c0a9eaf61198b356e7a0eb2
kiendey <141056025+kiendey@users.noreply.github.com>
2024-04-14 21:38:28 +0700

    Add files via upload

---
+++ cntt.js
@@ -0,0 +1,263 @@
+let mat_hangCNTT=[
+    {
+        id: 1,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/1sopptk.jpg',
+        tenGiaoTrinh :'Giáo trình một số PP tìm kiếm',
+        gia : 100000
+    },
+    {
+        id: 2,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/asp.net.jpg',
+        tenGiaoTrinh :'Giáo trình ASP.NET',
+        gia : 900000
+    },
+    {
+        id: 3,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/c++.jpg',
+        tenGiaoTrinh :'Ngôn ngữ lập trinh C++',
+        gia : 50000
+    },
+    {
+        id: 4,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/cnpm.jpg',
+        tenGiaoTrinh :'Giáo trình nhập môn CNPM',
+        gia : 55000
+    },
+    {
+        id: 5,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/congNghe_portal.jpg',
+        tenGiaoTrinh :'Giáo trình công nghệ portal',
+        gia : 45000
+    },
+    {
+        id: 6,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/congnghexml.jpg',
+        tenGiaoTrinh :'Giáo trình công nghệ XML',
+        gia : 51000
+    },
+    {
+        id: 7,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/csdl.jpg',
+        tenGiaoTrinh :'Giáo trình cơ sở dữ liệu',
+        gia : 56000
+    },
+    {
+        id: 8,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/csnhung.jpg',
+        tenGiaoTrinh :'Giáo trình cơ sở lập trình nhúng',
+        gia : 50000
+    },
+    {
+        id: 9,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ctdl.jpg',
+        tenGiaoTrinh :'Giáo trình cấu trúc dữ liệu và giải thuật',
+        gia : 100000
+    },
+    {
+        id: 10,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/dhud.jpg',
+        tenGiaoTrinh :'Giáo trình đồ họa ứng dụng',
+        gia : 30000
+    },
+    {
+        id: 11,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/dhmt.jpg',
+        tenGiaoTrinh :'Giáo trình đồ họa máy tính',
+        gia : 80000
+    },
+    {
+        id: 10,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/hcsdl.jpg',
+        tenGiaoTrinh :'Giáo trình hệ cơ sở dữ liệu',
+        gia : 90000
+    },
+    {
+        id: 11,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/hcg.jpg',
+        tenGiaoTrinh :'Giáo trình hệ chuyên gia',
+        gia : 56000
+    },
+    {
+        id: 12,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/java.jpg',
+        tenGiaoTrinh :'Ngôn ngữ lập trình Java',
+        gia : 55000
+    },
+    {
+        id: 13,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/kpdl.jpg',
+        tenGiaoTrinh :'Giáo trình khai phá dữ liệu',
+        gia : 33000
+    },
+    {
+        id: 14,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ktlt.jpg',
+        tenGiaoTrinh :'Giáo trình kỹ thuật lập trình',
+        gia : 66000
+    },
+    {
+        id: 15,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ktmt.jpg',
+        tenGiaoTrinh :'Giáo trình kiến trúc máy tính',
+        gia : 54000
+    },
+    {
+        id: 16,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/laptrinhwindow.jpg',
+        tenGiaoTrinh :'Giáo trình lập trình windows',
+        gia : 58000
+    },
+    {
+        id: 17,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ltnhung.jpg',
+        tenGiaoTrinh :'Lập trình nhúng (sách tham khỏa)',
+        gia : 72000
+    },
+    {
+        id: 18,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ltungdung.jpg',
+        tenGiaoTrinh :'Giáo trình LT tập thô và ứng dụng',
+        gia : 87000
+    },
+    {
+        id: 19,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/mangmaytinh.jpg',
+        tenGiaoTrinh :'Giáo trình mạng máy tính',
+        gia : 63000
+    },
+    {
+        id: 20,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/oop_java.jpg',
+        tenGiaoTrinh :'Giáo trình OOP với Java',
+        gia : 74000
+    },
+    {
+        id: 21,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ptpm.jpg',
+        tenGiaoTrinh :'Giáo trình công cụ PTPM',
+        gia : 45000
+    },
+    {
+        id: 22,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/pttkhdt.jpg',
+        tenGiaoTrinh :'Giáo trình phân tích thiết kế OOP',
+        gia : 60000
+    },
+    {
+        id: 23,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/quantrimang.jpg',
+        tenGiaoTrinh :'Giáo trình quản trị mạng',
+        gia : 70000
+    },
+    {
+        id: 24,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/thietkeweb.jpg',
+        tenGiaoTrinh :'Giáo trình thiết kế Website',
+        gia : 100000
+    },
+    {
+        id: 25,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ToiUuHoa.jpg',
+        tenGiaoTrinh :'Giáo trình tối ưu hóa',
+        gia : 100000
+    },
+    {
+        id: 26,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/ttnt.jpg',
+        tenGiaoTrinh :'Giáo trình trí tệu nhân tạo',
+        gia : 100000
+    },
+    {
+        id: 27,
+        anh : './Anh/CÔNG NGHỆ THÔNG TIN/xla.jpg',
+        tenGiaoTrinh :'Giáo trình xử lí ảnh',
+        gia : 100000
+    }
+]
+let mathangCNTT=document.querySelector('#mat_hang');
+let cuaSo_muahang=document.querySelector('#cuaSo_muahang');
+function renderSp(matHang,sanpham,fn){
+    let html=matHang.map(function(matHang){
+    return `
+    <div class="matHang">
+        <img src="${matHang.anh}" alt="">
+        <p id="tenSanPham">${matHang.tenGiaoTrinh}</p>
+        <h4 id="giaTien">${matHang.gia}đ</h4>
+        <div id="themVaoGioHang" onclick="xuLiMuaHang(${fn},${matHang.id})">THÊM VÀO GIỎ HÀNG</div>
+    </div>
+    `
+})
+    sanpham.innerHTML=html.join('');
+}
+function Close(){
+    let cuaSo_muahang=document.querySelector('#cuaSo_muahang')
+    let close=cuaSo_muahang.querySelector('i')
+    cuaSo_muahang.classList.remove('active')
+}
+function xuLiGioHang(matHang){
+    let api='http://localhost:3000/gioHang'
+    let options = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(matHang)
+      }
+      fetch(api,options)
+        .then(function(res){
+          return res.json()
+        })
+        .then(function(a){
+            console.log(a)
+        })
+}
+function xuLiMuaHang(matHang,id){
+    let myPromise = new Promise(function(myResolve, myReject) {
+        let x = 0;
+      
+      // The producing code (this may take some time)
+      
+        if (x == 0) {
+          myResolve("OK");
+        } else {
+          myReject("Error");
+        }
+      });
+      myPromise.then(
+        function(){
+            let thongTinSP= cuaSo_muahang.querySelector('#hienThi')
+            cuaSo_muahang.classList.add('active')
+            let temp={};
+            matHang.forEach(function(matHang,index){
+            if(matHang.id===id){
+            temp={
+                tenGiaoTrinh : matHang.tenGiaoTrinh,
+                gia : matHang.gia,
+                anh : matHang.anh
+            };
+            thongTinSP.innerHTML=`
+            <img src="${matHang.anh}" alt="">
+            <div id="thongTin_SP">
+                <h3 id="tenSP">${matHang.tenGiaoTrinh}</h3>
+                <h2 id="gia">Giá : ${matHang.gia} Đ</h2>
+                <div>Số lượng : <input type="number" id="soLuongSP" min="1" value="1")"></div>
+                <button type="button" id="click_mua">Cho vào giỏ hàng</button>
+            </div>
+            <i class="fa-solid fa-circle-xmark dongCuaSo" onclick="Close()"></i>
+            `
+        }
+    })
+        return temp;
+        }
+      )
+    .then(function(temp){
+        let muaHang = document.querySelector('#click_mua')
+        if(muaHang) muaHang.onclick=function(){
+            cuaSo_muahang.classList.remove('active')
+            temp.soLuong=document.querySelector('#soLuongSP').value
+            xuLiGioHang(temp)
+        }
+    })
+}
+renderSp(mat_hangCNTT,mathangCNTT,'mat_hangCNTT');
+
